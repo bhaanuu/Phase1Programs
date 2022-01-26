014be0e1879e6ef8b850de515836220e99bc0345
bhaanuu <dugganabhanu@gmail.com>
2022-01-26 20:19:11 +0530

    Committing

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,29 +1,32 @@
-
+import java.util.Scanner;
 public class Calculator {
-	int add(int a, int b)
+	int Add(int a, int b)
 	{
 		return a+b;
 	}
-	int sub(int a, int b)
+	int Subtraction(int a, int b)
 	{
 		return a-b;
 	}
-	int div(int a, int b)
+	int Multiply(int a, int b)
+	{
+		return a*b;
+	}
+	int Division(int a, int b)
 	{
 		return a/b;
 	}
-	int mod(int a, int b)
-	{
-		return a%b;
-	}
-
 	public static void main(String[] args) 
 	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		Calculator c = new Calculator();
-		System.out.println(c.add(5,5));
-		System.out.println(c.sub(20,10));
-		System.out.println(c.div(100,10));
-		System.out.println(c.mod(10,10));
+		System.out.println(c.Add(a,b));
+		System.out.println(c.Subtraction(a,b));
+		System.out.println(c.Multiply(a,b));
+		System.out.println(c.Division(a,b));
+		
 	}
 
 }
