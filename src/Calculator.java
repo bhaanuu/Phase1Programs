5e7353e66c1dd3d85dcb919b6d568229f81944ed
bhaanuu <dugganabhanu@gmail.com>
2022-01-27 13:35:26 +0530

    Committing

--- src/Calculator.java
+++ src/Calculator.java
@@ -19,14 +19,24 @@ public class Calculator {
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Operation \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division ");
+		int operation = sc.nextInt();
+		if(operation < 5)
+		{
+		System.out.println("Enter two values");
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		Calculator c = new Calculator();
-		System.out.println("Addition:"+c.Add(a,b));
-		System.out.println("Subtraction"+c.Subtraction(a,b));
-		System.out.println("Multiply"+c.Multiply(a,b));
-		System.out.println("Division"+c.Division(a,b));
-		
+		if(operation == 1)
+		System.out.println("Addition: "+c.Add(a,b));
+		if(operation == 2)
+		System.out.println("Subtraction: "+c.Subtraction(a,b));
+		if(operation == 3)
+		System.out.println("Multiply: "+c.Multiply(a,b));
+		if(operation == 4)
+		System.out.println("Division: "+c.Division(a,b));
+		}
+		else
+		System.out.println("Invalid input");	
 	}
-
 }
