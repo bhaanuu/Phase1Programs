f68c647627cfb688adb4f8ad0ad91a6a794713dd
bhaanuu <dugganabhanu@gmail.com>
2022-02-05 13:39:04 +0530

    Committing

---
+++ src/StringConversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class StringConversion 
+{
+	public static void main(String[] args) {
+		
+	
+	String s = "Name";
+	StringBuffer s1= new StringBuffer(s);
+	StringBuilder s2 = new StringBuilder(s);
+	System.out.println(s1);
+	System.out.println(s2);
+	
+	}
+	
+
+}
